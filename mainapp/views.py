7e7ee0846a62827cb01373b3917e3fc6b8416139
Gontarenok <vika030395@gmail.com>
2022-10-05 04:10:28 +0300

    Add urls, templates, static

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+# class HelloWorldView(View):
+#
+#     def get(self, request, *args, **kwargs):
+#         return HttpResponse("Hello world!")
+#
+# # def hello_world(request):
+# #     return HttpResponse("Hello world!")
+#
+# def blog(request, **kwargs):
+#     return HttpResponse(f'{kwargs}')
+
+class ContactsView(TemplateView):
+    template_name = 'mainapp/contacts.html'
+
+class CoursesListView(TemplateView):
+    template_name = 'mainapp/courses_list.html'
+
+class DocSiteView(TemplateView):
+    template_name = 'mainapp/doc_site.html'
+
+class IndexView(TemplateView):
+    template_name = 'mainapp/index.html'
+
+class LoginView(TemplateView):
+    template_name = 'mainapp/login.html'
+
+class NewsView(TemplateView):
+    template_name = 'mainapp/news.html'
